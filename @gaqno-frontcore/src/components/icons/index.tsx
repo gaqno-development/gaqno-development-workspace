f44656e0def7cde105e6a229ee23907913d5ee61
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-02-01 21:11:45 -0300

    chore: bump @gaqno-development/frontcore to 1.0.22, add components/icons (published)

---
+++ @gaqno-frontcore/src/components/icons/index.tsx
@@ -0,0 +1,58 @@
+import {
+  Settings,
+  Target,
+  Users,
+  ShoppingCart,
+  Book,
+  Volume2,
+  ShieldCheck,
+  FileText,
+  Pencil,
+  Paintbrush,
+  ChevronDown,
+  ChevronRight,
+  User,
+  Trash2,
+  Sparkles,
+  ArrowLeft,
+  RotateCw,
+  Copy,
+  Check,
+  Info,
+  SendHorizontal,
+  X,
+  Search,
+  TriangleAlert,
+  Lightbulb,
+  Home,
+  Bell,
+  type LucideIcon,
+} from "lucide-react";
+
+export const HomeIcon: LucideIcon = Home;
+export const FilledBellIcon: LucideIcon = Bell;
+export const GearIcon: LucideIcon = Settings;
+export const TargetIcon: LucideIcon = Target;
+export const UsersIcon: LucideIcon = Users;
+export const ShoppingCartIcon: LucideIcon = ShoppingCart;
+export const BookIcon: LucideIcon = Book;
+export const Volume2Icon: LucideIcon = Volume2;
+export { ShieldCheck };
+export const FileDescriptionIcon: LucideIcon = FileText;
+export const PenIcon: LucideIcon = Pencil;
+export const PaintIcon: LucideIcon = Paintbrush;
+export const DownChevron: LucideIcon = ChevronDown;
+export const RightChevron: LucideIcon = ChevronRight;
+export const UserIcon: LucideIcon = User;
+export const TrashIcon: LucideIcon = Trash2;
+export const SparklesIcon: LucideIcon = Sparkles;
+export const ArrowNarrowLeftIcon: LucideIcon = ArrowLeft;
+export const RefreshIcon: LucideIcon = RotateCw;
+export const CopyIcon: LucideIcon = Copy;
+export const CheckedIcon: LucideIcon = Check;
+export const InfoCircleIcon: LucideIcon = Info;
+export const SendHorizontalIcon: LucideIcon = SendHorizontal;
+export const XIcon: LucideIcon = X;
+export const MagnifierIcon: LucideIcon = Search;
+export const TriangleAlertIcon: LucideIcon = TriangleAlert;
+export const BulbSvg: LucideIcon = Lightbulb;
